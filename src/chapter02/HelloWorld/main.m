5e5f1b53de19330ed5436d6b6a0a952a7c8c58d5
Atit Patumvan <atit@patumvan.com>
2012-04-15 03:06:19 +0700

    simple program example

---
+++ src/chapter02/HelloWorld/main.m
@@ -0,0 +1,13 @@
+// Program 2.1
+// First program example
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, char *argv[]) {
+	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+	NSLog(@"Hello, World!");
+	
+	[pool drain];
+	[return 0];
+	}
+}
